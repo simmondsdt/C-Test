954d2e17bc3d446689cf6b0fc692e336528bbc03
Daniel Simmonds <simmondsdt@gmail.com>
2017-05-10 11:36:04 -0600

    initial commit

---
+++ hello_world/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program {
+	static void Main() {
+		Console.Write("Hello, what is your name: ");
+		string username = Console.ReadLine();
+		Console.Write("Hello " + username + " do you like fruitcake? ");
+		string answer = Console.ReadLine();
+		if (answer == "yes" || answer == "Yes") {
+			Console.Write("You suck");
+			Environment.Exit(0);
+		} else {
+			Console.Write("You rock!");
+			Environment.Exit(0);
+		};
+	}
+}
